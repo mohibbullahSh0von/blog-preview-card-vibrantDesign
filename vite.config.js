b3d9017f8a613a2fac56c1b839bcbd717fd14b48
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-04-28 15:36:20 +0600

    deployment modifications

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/blog-preview-card-vibrantDesign/",
   plugins: [react(), tailwindcss()],
 })
